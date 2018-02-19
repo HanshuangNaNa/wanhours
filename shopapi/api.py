b848ffd4a6acd81260fbe0845a8c1ef6d9ee452a
HanshuangNaNa <hanshuangnana@gmail.com>
2018-02-19 19:38:20 +0800

    daily

--- shopapi/api.py
+++ shopapi/api.py
@@ -7,25 +7,54 @@
 # Description:
 """
 import logging
-from flask import Flask,request
-from flask.ext.restful import Resource,Api
+from flask import Flask,request,jsonify,abort,make_response
 
-logging.basicConfig(level=logging.INFO,format='%(asctime)s %(filename)s : %(levelname)s %(message)s',datefmt='%Y-%m-%d %A %H:%M:%S')
 app = Flask(__name__)
-api = Api(app)
 
-todos = {}
+tasks = [
+        {
+            'id':1,
+            'title':'coding',
+            'description':'一万个小时',
+            'done':False
+        },
+        {
+            'id':2,
+            'title':'写商城',
+            'description':'写接口',
+            'done':False
+        }
+        ]
 
-class TodoSimple(Resource):
-    def get(self,todo_id):
-        return {todo_id:todos[todo_id]}
+@app.route('/todo/tasks',methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks':tasks})
 
-    def put(self,todo_id):
-        todos[todo_id] = request.form['data']
-        return {todo_id:todos[todo_id]}
+@app.route('/todo/tasks/<int:task_id>',methods=['GET'])
+def get_task(task_id):
+    task = list(filter(lambda t : t['id'] == task_id,tasks))
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task':task[0]})
 
-api.add_resource(TodoSimple,'/<string:todo_id>')
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error':'Not found'}),404)
+
+@app.route('/todo/tasks',methods=['POST'])
+def create_task():
+    if not request.json or not 'title' in request.json:
+        abort(400)
+    task = {
+            'id':tasks[-1]['id']+1,
+            'title':request.json['title'],
+            'description':request.json.get('description',""),
+            'done':False
+            }
+    tasks.append(task)
+    return jsonify({'task':task}),201
 
 if __name__ == '__main__':
     app.run(debug=True)
-    app.run
+
+
